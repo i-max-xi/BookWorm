2631bd142d70244ef77c326c9ee1b00fc19f9613
Virag Kormoczy <vkormoczy@gmail.com>
2022-07-19 08:22:58 +0300

    Add navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/navbar.css";
+import logo from "../planet.png";
+
+const Navbar = () => (
+  <header>
+    <nav>
+      <div className="logoContainer">
+        <img src={logo} alt="planet app logo" className="logo" />
+        <h1>
+          <NavLink to="/">Space Traveler&apos;s Hub</NavLink>
+        </h1>
+      </div>
+      <ul className="navLinks">
+        <li>
+          <NavLink to="/">Rockets</NavLink>
+        </li>
+        <li>
+          <NavLink to="missions">Missions</NavLink>
+        </li>
+        <li>
+          <div className="divider" />
+        </li>
+        <li>
+          <NavLink to="profile">My Profile</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Navbar;
